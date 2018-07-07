c59a6a8b68c090a3a000c02af43b8d98de1abed0
YashBharadwaj12 <ybharadwaj12@gmail.com>
2018-07-08 01:18:53 +0530

    Implement Flickr image

--- flickrImageSearch/flickrImageSearch/Model/FISFlickrImage.m
+++ flickrImageSearch/flickrImageSearch/Model/FISFlickrImage.m
@@ -8,6 +8,36 @@
 
 #import "FISFlickrImage.h"
 
+static NSString * const ImageURL = @"http://farm%@.static.flickr.com/%@/%@_%@.jpg";
+
+@interface FISFlickrImage ()
+
+@property (nonatomic, readonly) NSString *imageId;
+@property (nonatomic, readonly) NSString *title;
+@property (nonatomic, readonly) NSString *farm;
+@property (nonatomic, readonly) NSString *server;
+@property (nonatomic, readonly) NSString *secret;
+@end
+
 @implementation FISFlickrImage
+
+- (instancetype)initWithJSON:(NSDictionary *)JSON {
+    NSAssert(JSON != nil, @"JSON is nil");
+    self = [super init];
+    if (self) {
+        _imageId = [JSON objectForKey:@"id"];
+        _title = [JSON objectForKey:@"title"];
+        _farm = [JSON objectForKey:@"farm"];
+        _server = [JSON objectForKey:@"server"];
+        _secret = [JSON objectForKey:@"secret"];
+    }
+    
+    return nil;
+}
+
+- (NSURL *)imageRemoteURL {
+    NSString *stringURL = [NSString stringWithFormat:ImageURL, self.farm, self.server, self.imageId, self.secret];
+    return [NSURL URLWithString:stringURL];
+}
 
 @end
